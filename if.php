198f8858bb03ded1e2610f6424e57946fbd21e44
Lori Marshall <lorettamarshall123@yahoo.com>
2014-05-09 12:00:55 -0500

    if elseif else

--- if.php
+++ if.php
@@ -1,46 +1,42 @@
 <?php
 
-
 $a = 5;
 $b = 10;
 $c = '10';
 
+
 if ($a < $b) {
-    // output the appropriate result
-    echo "$a is less than $b\n";
+    
+    echo "$a is less than $b";
+} else {
+
+    echo "$b is less than $a";
 }
 
-if ($b > $a) {
-    // output the appropriate result
-    echo "$b is greater $a\n";
-}
-
+// Shorten the next 2 statements into an if/else
 if ($b >= $c) {
-    // output the appropriate result
-    echo "$b is great than or equal to $c\n";
+
+    echo "$b is greater than or equal to $c\n";
 }
 
-if ($b <= $c) {
-    // output the appropriate result
+else {
+    
     echo "$b is less than or equal to $c\n";
 }
 
-if ($b == $c) {
-    // output the appropriate result
-    echo "$b is equal to $c\n";
-}
 
 if ($b === $c) {
-    // output the appropriate result
-    echo "$b is the same as $c\n";
+    echo "$b is identical to $c";
+}
+    elseif ($b == $c) {
+    echo "$b is equal to $c";
+}
+    elseif ($b !== $c) {
+    echo "$b is not identical to $c";
+}
+    else {
+    echo "$b is not equal to $c";
+
 }
 
-// output the appropriate result
-if ($b != $c) {
-    echo "$b is not equal to $c\n";
-}
-
-// output the appropriate result
-if ($b !==$c) {
-    echo "$b is not identical to $c\n";
-}
+?>
