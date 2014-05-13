217ec5e36f174d7ea0ee0f5e282474e609cc8cb7
Lori Marshall <lorettamarshall123@yahoo.com>
2014-05-13 10:56:02 -0500

    added numeric argument

--- for.php
+++ for.php
@@ -1,21 +1,33 @@
 <?php
 
 fwrite (STDOUT, "Give me a starting number\n");
-$start = (int) fgets(STDIN);
+$start = trim(fgets(STDIN));
+
+if (!is_numeric($start)){
+	echo "Must be numeric.\n";
+	exit (0);
+}
 
 fwrite (STDOUT, "Give me an ending number\n");
-$end = (int) fgets(STDIN);
+$end = trim(fgets(STDIN));
+
+if (!is_numeric($end)){
+	echo "Must be numeric.\n";
+	exit (0);
+}
 
 fwrite (STDOUT, "Give me a number to increment by\n");
-$increment = (int) fgets(STDIN);
+$increment = trim(fgets(STDIN));
+
+if (!is_numeric($increment)){
+	echo "Must be numeric.\n";
+	exit (0);
+}
 
 if ($increment == 0) {
 	$increment = 1;
 }
-	
 
 for($b = $start; $b <= $end; $b += $increment){
 	echo "\$b has a value of {$b}\n" . PHP_EOL;
 }
-
-
