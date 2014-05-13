b41556d2ff1754d3a5bf68852d4df59836f534cd
Lori Marshall <lorettamarshall123@yahoo.com>
2014-05-13 10:38:48 -0500

    added default for no incrementor

--- for.php
+++ for.php
@@ -6,8 +6,13 @@ $start = (int) fgets(STDIN);
 fwrite (STDOUT, "Give me an ending number\n");
 $end = (int) fgets(STDIN);
 
-fwrite (STDOUT, "Give me a number to incremnt by\n");
+fwrite (STDOUT, "Give me a number to increment by\n");
 $increment = (int) fgets(STDIN);
+
+if ($increment == 0) {
+	$increment = 1;
+}
+	
 
 for($b = $start; $b <= $end; $b += $increment){
 	echo "\$b has a value of {$b}\n" . PHP_EOL;
