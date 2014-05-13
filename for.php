1db635f962ee367b283e8d0a6d6a254329007038
Lori Marshall <lorettamarshall123@yahoo.com>
2014-05-13 10:30:12 -0500

    added incrementor

--- for.php
+++ for.php
@@ -6,6 +6,11 @@ $start = (int) fgets(STDIN);
 fwrite (STDOUT, "Give me an ending number\n");
 $end = (int) fgets(STDIN);
 
-for($b = $start; $b <= $end; $b++){
+fwrite (STDOUT, "Give me a number to incremnt by\n");
+$increment = (int) fgets(STDIN);
+
+for($b = $start; $b <= $end; $b += $increment){
 	echo "\$b has a value of {$b}\n" . PHP_EOL;
 }
+
+
