5d1df453e2557e9da7621d3ec80bc927fcdb18bc
Lori Marshall <lorettamarshall123@yahoo.com>
2014-05-14 11:13:20 -0500

    added break

--- break_continue.php
+++ break_continue.php
@@ -1,8 +1,15 @@
 <?php
 
-for ($i = 0; $i <= 100; $i++) {
-	if ($i % 2 == 0){
+// for ($i = 0; $i <= 100; $i++) {
+// 	if ($i % 2 == 0){
+// 	echo $i . PHP_EOL;
+// 		continue;
+// 	} 
+// }
+
+for ($i = 1; $i <= 100; $i++) {
 	echo $i . PHP_EOL;
-		continue;
-	} 
+	if ($i == 10) {
+		break;
+	}
 }
