6aaa6d02f078f9601a5c00011469e7cd2cfb440a
Lori Marshall <lorettamarshall123@yahoo.com>
2014-05-14 08:20:52 -0500

    included for loop

--- fruits.php
+++ fruits.php
@@ -1,8 +1,20 @@
 <?php
 
-$fruits = array ('apples', 'oranges', 'bananas');
+$fruits = array (
+		'apples', 
+		'oranges', 
+		'bananas',
+		'kiwi',
+		'cantaloupe'
+		);
 
-foreach ($fruits as $key => $value) {
-		echo "Fruit is $value\n";
+// foreach ($fruits as $key => $value) {
+// 		echo "Fruit is $value\n";
+// }
+
+
+for ($i =0 ; $i  < count($fruits) ; $i++ ) { 
+	echo "Fruit name $fruits[$i]\n";
 }
+
 
