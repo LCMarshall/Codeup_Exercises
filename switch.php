ff5eee31626971ce53cafa68cf0023b56714bed9
Lori Marshall <lorettamarshall123@yahoo.com>
2014-05-14 13:27:54 -0500

    converted to if

--- switch.php
+++ switch.php
@@ -5,28 +5,47 @@ date_default_timezone_set('America/Chicago');
 
 // Get Day of Week as number
 // 1 (for Monday) through 7 (for Sunday)
+// $day_of_week = date('N');
+
+// switch($day_of_week) {
+//     case 1:
+//         echo 'Monday' . PHP_EOL;
+//         break;
+//     case 2:
+//         echo 'Tuesday' . PHP_EOL;
+//         break;
+//     case 3:
+//         echo 'Wednesday' . PHP_EOL;
+//         break;
+//     case 4:
+//         echo 'Thursday' . PHP_EOL;
+//         break;
+//      case 5:
+//         echo 'Friday' . PHP_EOL;
+//         break;
+//      case 6:
+//         echo 'Saturday' . PHP_EOL;
+//         break;
+//      case 7:
+//         echo 'Sunday' . PHP_EOL;
+//         break;
+// }
+
+
 $day_of_week = date('N');
 
-switch($day_of_week) {
-    case 1:
-        echo 'Monday' . PHP_EOL;
-        break;
-    case 2:
-        echo 'Tuesday' . PHP_EOL;
-        break;
-    case 3:
-        echo 'Wednesday' . PHP_EOL;
-        break;
-    case 4:
-        echo 'Thursday' . PHP_EOL;
-        break;
-     case 5:
-        echo 'Friday' . PHP_EOL;
-        break;
-     case 6:
-        echo 'Saturday' . PHP_EOL;
-        break;
-     case 7:
-        echo 'Sunday' . PHP_EOL;
-        break;
+if ($day_of_week == 1) {
+    echo 'Monday';
+} elseif ($day_of_week == 2) {
+    echo 'Tuesday';
+} elseif ($day_of_week == 3) {
+    echo 'Wednesday';
+} elseif ($day_of_week == 4) {
+    echo 'Thursday';
+} elseif ($day_of_week == 5) {
+    echo 'Friday';
+} elseif ($day_of_week == 6) {
+    echo 'Saturday';
+}elseif ($day_of_week == 7) {
+    echo 'Sunday'; 
 }
