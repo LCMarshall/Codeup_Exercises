e9d63b7cb6d2622ba85f2e3f752d83752a54718a
Lori Marshall <lorettamarshall123@yahoo.com>
2014-05-15 16:14:00 -0500

    added return

--- arithmetic.php
+++ arithmetic.php
@@ -9,74 +9,66 @@
 // Refactor the error messages into their own function, have the other functions use it for error messaging.
 
 
-
-function add($a, $b) {
-	if(is_numeric($a) && is_numeric ($b)){
-		echo $a + $b;
-	}else {
-		echo "Error: both {$a} and {$b} should be numbers";
-	}
-	echo PHP_EOL;    
-}
-
-function subtract($a, $b) {
-    if(is_numeric($a) && is_numeric ($b)){
-		echo $a - $b;
-	}else {
-		echo "Error: both {$a} and {$b} should be numbers";
+function error_message ($a, $b) {
+	if (!(is_numeric($a) && is_numeric($b))) {
+		return false;
+		} else { 
+			echo "Error: both {$a} and {$b} should be numbers";
+			return true;
 	}
 	echo PHP_EOL;
+}
+
+function add($a, $b) {
+	if (is_numeric($a) && is_numeric($b)){
+		return $a + $b;
+	} else {
+		return error_message($a, $b);
+	echo PHP_EOL;    
+}
+}
+function subtract($a, $b) {
+    if (is_numeric($a) && is_numeric($b)){
+		return $a - $b;
+	} else {
+		return error_message($a, $b);
+	echo PHP_EOL;
+}
 }
 
 function multiply($a, $b) {
-    if(is_numeric($a) && is_numeric ($b)){
-		echo $a * $b;
-	}else {
-		echo "Error: both {$a} and {$b} should be numbers";
-	}
+    if (is_numeric($a) && is_numeric($b)){
+		return $a * $b;
+	} else {
+		return error_message($a, $b);
 	echo PHP_EOL;
+}
 }
 
 function divide($a, $b) {
     if ($b == 0 && is_numeric ($b)) {
-    	echo "Cannot divide by zero";
-	} else{
-		if (is_numeric($a) && is_numeric ($b)){
-		echo $a / $b;}
-		else {
-		echo "Error: both {$a} and {$b} should be numbers";
+    	return "Cannot divide by zero";
+	} else {
+		if (is_numeric($a) && is_numeric($b)){
+			return $a / $b;
+		} else {
+			return error_message($a, $b);
 		}
-	}
 	echo PHP_EOL;
-}
-	
+	}
+}	
 
 function modulus($a, $b) {
-	if(is_numeric($a) && is_numeric ($b)){
-		echo $a % $b;
-	}else {
-		echo "Error: both {$a} and {$b} should be numbers";
-	}
-	echo PHP_EOL;
-}
-
-function error_message ($a, $b) {
-	if(!(is_numeric($a) && is_numeric ($b))) {
-		echo "Error: both {$a} and {$b} should be numbers";
-	}
-	echo PHP_EOL;
-}
-
-function test($a, $b) {
-	if(is_numeric($a) && is_numeric ($b)){
-		echo $a + $b;
+	if (is_numeric($a) && is_numeric($b)){
+		return $a % $b;
 	} else {
-		echo error_message($a, $b);
-	}
-	echo PHP_EOL;    
-}	
-test (1, test);
-// // subtract (10, a);
+		return error_message($a, $b);
+	echo PHP_EOL;
+	}	
+}
+
+echo add (10, 1);
+echo subtract (10, 1);
 // // multiply (2, b);
 // divide (2, test);
 // // modulus (10, d);
